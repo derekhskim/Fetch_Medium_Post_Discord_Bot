7942329c1819820934822ec0b8916f4a0794808b
noobietubie <85812317+treasure3210@users.noreply.github.com>
2023-08-28 23:34:02 -0600

    use sqlite3 instead of creating a new .json

--- fetch_medium_post.js
+++ fetch_medium_post.js
@@ -3,12 +3,14 @@ const feedparser = require('feedparser-promised');
 const dotenv = require('dotenv');
 const express = require('express');
 
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./last_post.db'); // Using a file-based SQLite database
+
+// Create a table to store the last post
+db.run("CREATE TABLE IF NOT EXISTS last_post (title TEXT, link TEXT)");
+
 // Load environment variables from .env file
 dotenv.config();
-
-const fs = require('fs');
-
-const LAST_POST_FILE = './last_post.json';
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
@@ -27,25 +29,26 @@ async function checkMediumFeed() {
 
         let lastPost = { title: '', link: '' };
         
-        // Load last post details if the file exists
-        if (fs.existsSync(LAST_POST_FILE)) {
-            const rawData = fs.readFileSync(LAST_POST_FILE, 'utf-8');
-            lastPost = JSON.parse(rawData);
-        }
-
-        // If the latest fetched post is different from the last known post
-        if (latestFeedItem.link !== lastPost.link || latestFeedItem.title !== lastPost.title) {
-            const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
-            if (channel) {
-                channel.send(`📢 New Blog Post: ${latestFeedItem.title} - ${latestFeedItem.link}`);
+        // Retrieve the last post from SQLite
+        db.get("SELECT title, link FROM last_post", [], (err, row) => {
+            if (err) {
+                return console.error('Error fetching from SQLite:', err.message);
             }
-            // Update the last known post details
-            fs.writeFileSync(LAST_POST_FILE, JSON.stringify({
-                title: latestFeedItem.title,
-                link: latestFeedItem.link
-            }));
-        }
-
+            if (row) {
+                lastPost = { title: row.title, link: row.link };
+            }
+            
+            // If the latest fetched post is different from the last known post
+            if (latestFeedItem.link !== lastPost.link || latestFeedItem.title !== lastPost.title) {
+                const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
+                if (channel) {
+                    channel.send(`📢 New Blog Post: ${latestFeedItem.title} - ${latestFeedItem.link}`);
+                }
+                // Update the last known post details in SQLite
+                db.run("DELETE FROM last_post");
+                db.run("INSERT INTO last_post(title, link) VALUES(?, ?)", [latestFeedItem.title, latestFeedItem.link]);
+            }
+        });
     } catch (err) {
         console.error('Failed to fetch Medium feed:', err);
     }
