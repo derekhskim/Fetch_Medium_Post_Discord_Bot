f0a36e74966351bcf1fa7746fd47b546e7b8adf4
noobietubie <85812317+treasure3210@users.noreply.github.com>
2023-08-29 00:30:58 -0600

    Use ElephantSQL to keep track of table stored data rather than relying on sqlite3 as it will drop tables when Glitch platform fall asleep

--- fetch_medium_post.js
+++ fetch_medium_post.js
@@ -2,15 +2,21 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const feedparser = require('feedparser-promised');
 const dotenv = require('dotenv');
 const express = require('express');
-
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('./last_post.db'); // Using a file-based SQLite database
-
-// Create a table to store the last post
-db.run("CREATE TABLE IF NOT EXISTS last_post (title TEXT, link TEXT)");
+const { Pool } = require('pg');
 
 // Load environment variables from .env file
 dotenv.config();
+
+// Setup the PostgreSQL connection pool
+const pool = new Pool({
+    connectionString: process.env.ELEPHANTSQL_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+
+// Ensure the table is created
+pool.query("CREATE TABLE IF NOT EXISTS last_post (title TEXT, link TEXT)");
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
@@ -19,36 +25,29 @@ const client = new Client({
 client.once('ready', () => {
     console.log('Bot is running!');
     checkMediumFeed();
-    setInterval(checkMediumFeed, 60000);  // Check every 1 minute
+    setInterval(checkMediumFeed, 60000);
 });
 
 async function checkMediumFeed() {
     try {
         const articles = await feedparser.parse(process.env.MEDIUM_RSS_URL);
         const latestFeedItem = articles[0];
-
-        let lastPost = { title: '', link: '' };
         
-        // Retrieve the last post from SQLite
-        db.get("SELECT title, link FROM last_post", [], (err, row) => {
-            if (err) {
-                return console.error('Error fetching from SQLite:', err.message);
+        // Retrieve the last post from ElephantSQL
+        const { rows } = await pool.query("SELECT title, link FROM last_post");
+        const lastPost = rows[0] || { title: '', link: '' };
+
+        // If the latest fetched post is different from the last known post
+        if (latestFeedItem.link !== lastPost.link || latestFeedItem.title !== lastPost.title) {
+            const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
+            if (channel) {
+                channel.send(`📢 New Blog Post Alert!\n${latestFeedItem.title}\n${latestFeedItem.link}`);
             }
-            if (row) {
-                lastPost = { title: row.title, link: row.link };
-            }
-            
-            // If the latest fetched post is different from the last known post
-            if (latestFeedItem.link !== lastPost.link || latestFeedItem.title !== lastPost.title) {
-                const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
-                if (channel) {
-                    channel.send(`📢 New Blog Post: ${latestFeedItem.title} - ${latestFeedItem.link}`);
-                }
-                // Update the last known post details in SQLite
-                db.run("DELETE FROM last_post");
-                db.run("INSERT INTO last_post(title, link) VALUES(?, ?)", [latestFeedItem.title, latestFeedItem.link]);
-            }
-        });
+
+            // Update the last known post details in ElephantSQL
+            await pool.query("DELETE FROM last_post");
+            await pool.query("INSERT INTO last_post(title, link) VALUES($1, $2)", [latestFeedItem.title, latestFeedItem.link]);
+        }
     } catch (err) {
         console.error('Failed to fetch Medium feed:', err);
     }
